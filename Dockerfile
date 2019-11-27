b835ae9db951d710dd77c7d6e7ebf85589cb8e0b
itzikkaliga <itzikkaliga01@gmail.com>
2019-11-27 14:55:15 +0200

    last commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8
+FROM java:8
 COPY target/student-services-0.0.1-SNAPSHOT.jar student-services-0.0.1-SNAPSHOT.jar
 EXPOSE 8083
 CMD ["java", "-jar", "student-services-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/in28minutes/springboot/studentservices/resource/InventoryItemsController.java
+++ src/main/java/com/in28minutes/springboot/studentservices/resource/InventoryItemsController.java
@@ -3,10 +3,13 @@ package com.in28minutes.springboot.studentservices.resource;
 
 import java.util.List;
 
+import javax.validation.constraints.Min;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.springboot.studentservices.inventoryitems.InvItem;
@@ -40,7 +43,8 @@ public class InventoryItemsController {
 	notes="if the item number is valid\n"
 			+ "then it will return an item\n"
 			+ "if not it wiil return null")
-	public InvItem getItemByItemNum(long itemNum){
+	public InvItem getItemByItemNum(
+			@RequestParam(value = "Item number", required = true) long itemNum){
 			return itemRepo.getItem(itemNum);
 	}
 	
@@ -50,11 +54,15 @@ public class InventoryItemsController {
 			+ "the requsted number then\n"
 			+ "nothing will happen.\n"
 			+ "please give a positive number.")
-	public String withdrawQuant(long itemNum, int decAmount){
-		if(decAmount<1)
+	public String withdrawQuant(
+			@RequestParam(value = "Item number", required = true) long itemNum, 
+			@RequestParam(value = "Amount", required = true) int decAmount){
+		if(decAmount<0)
 			return "Only positive numbers";
 		InvItem item = itemRepo.getItem(itemNum);
 		if(item!=null) {
+			if(item.getAmount()<decAmount)
+				return "the wanted amount is to big";
 			item.decAmount(decAmount);
 			itemRepo.saveItem(item);
 			return "Successful withdraw quant";
@@ -69,8 +77,10 @@ public class InventoryItemsController {
 					+ "the requsted number then\n"
 					+ "nothing will happen.\n"
 					+ "please give a positive number.")
-	public String depositItem(long itemNum, int addAmount){
-		if(addAmount<1)
+	public String depositItem(
+			@RequestParam(value = "Item number", required = true) long itemNum, 
+			@RequestParam(value = "Amount", required = true)int addAmount){
+		if(addAmount<0)
 			return "Only positive numbers";
 		InvItem item = itemRepo.getItem(itemNum);
 		if(item!=null) {
@@ -88,7 +98,19 @@ public class InventoryItemsController {
 			notes = "if there is not an item with\n"
 					+ "the requsted number then\n"
 					+ "nothing will happen.")
-	public String addItem(long itemno, String name, int amount, String invCode){
+	public String addItem(
+	@RequestParam(value = "Item number", required = true) long itemno, 
+	@RequestParam(value = "Item Name", required = true) String name, 
+	@RequestParam(value = "Amount", required = true) int amount, 
+	@RequestParam(value = "Code", required = true)String invCode){
+		if(itemno<1)
+			return "need valid id";
+		if(name.isEmpty())
+			return "need valid name";
+		if(invCode.isEmpty())
+			return "need valid code";
+		if(amount<0)
+			return "need positive amount";
 		itemRepo.saveItem(new InvItem(itemno, name, amount, invCode));
 		return "item added successfuly";
 	}
@@ -98,7 +120,8 @@ public class InventoryItemsController {
 			notes = "if there is not an item with\n"
 					+ "the requsted number then\n"
 					+ "nothing will happen.")
-	public String deleteItem(long itemno){
+	public String deleteItem(
+			@RequestParam(value = "Item number", required = true) long itemno){
 		itemRepo.deleteItem(itemno);
 		return "The item has been deleted";
 	}
